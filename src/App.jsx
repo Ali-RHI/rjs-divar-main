fe2eddf85b25d50065c8ff5f8a24ff77ca381902
Ali Rouhi <alirhtg@gmail.com>
2024-05-27 13:48:56 +0330

    config react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
-import Router from "router/Router.jsx"
+import Router from 'router/Router.jsx';
+import defaultOptions from 'configs/reactQuery.js';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
+	const queryClient = new QueryClient({ defaultOptions });
 	return (
-		<BrowserRouter>
-			<Router />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+			<ReactQueryDevtools/>
+		</QueryClientProvider>
 	);
 }
 

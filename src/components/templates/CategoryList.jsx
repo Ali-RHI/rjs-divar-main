35a1c18e90a90c7e84d030748001caff0bbda362
Ali Rouhi <alirhtg@gmail.com>
2024-05-28 15:59:00 +0330

    adding styles to categories section in admin page

--- src/components/templates/CategoryList.jsx
+++ src/components/templates/CategoryList.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { getCategory } from '../../services/admin.js';
+import Loader from '../module/Loader.jsx';
+import styles from "./CategoryList.module.css"
 
 function CategoryList() {
 	const { data, isLoading } = useQuery({
@@ -7,7 +9,21 @@ function CategoryList() {
 		queryKey: ['get-categories'],
 	});
 	console.log({ data, isLoading });
-	return <div>CategoryList</div>;
+	return (
+		<div className={styles.list}>
+			{isLoading ? (
+				<Loader />
+			) : (
+				data.data.map((i) => (
+					<div key={i._id}>
+                        <img src={`${i.icon}.svg`} alt="" />
+						<h5>{i.name}</h5>
+                        <p>slug: {i.slug}</p>
+					</div>
+				))
+			)}
+		</div>
+	);
 }
 
 export default CategoryList;

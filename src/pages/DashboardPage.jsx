3cae543dc1a723dea7dacab59699bd2d055e3dfd
Ali Rouhi <alirhtg@gmail.com>
2024-05-28 18:36:51 +0330

    get user posts data

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,9 +1,11 @@
 import AddPost from '../components/templates/AddPost.jsx';
+import PostList from '../components/templates/PostList.jsx';
 
 function DashboardPage() {
 	return (
 		<div>
 			<AddPost />
+			<PostList />
 		</div>
 	);
 }

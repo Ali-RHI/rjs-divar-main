6187c88dddf4e91825c5cef6763005dc351cddea
Ali Rouhi <alirhtg@gmail.com>
2024-05-31 18:55:53 +0330

    adding remove functionality to my posts section

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -1,16 +1,28 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { getPosts } from 'services/user.js';
 import Loader from '../module/Loader.jsx';
 import styles from './PostList.module.css';
 import { sp } from 'utils/replaceNumber.js';
+import api from '../../configs/api.js';
+import toast from 'react-hot-toast';
 
 function PostList() {
+	const queryClient = useQueryClient();
+
 	const { data, isLoading } = useQuery({
 		queryKey: ['my-post-list'],
 		queryFn: getPosts,
 	});
 
-const deleteHandler=(e)=>{}
+
+	const deleteHandler = (id) => {
+		api.delete(`${import.meta.env.VITE_BASE_URL}post/delete/${id}`)
+			.then((res) => {
+				toast.success(res.data.message);
+				queryClient.invalidateQueries({ queryKey: ['my-post-list'] });
+			})
+			.catch((error) => toast.error('مشکلی پیش آمده است!'));
+	};
 
 	return (
 		<div className={styles.list}>
@@ -32,7 +44,12 @@ const deleteHandler=(e)=>{}
 								<span>{post.options.content}</span>
 							</div>
 							<div className={styles.delete}>
-								<button onClick={deleteHandler}>حذف آگهی</button>
+								<button
+									onClick={() => {
+										deleteHandler(post._id);
+									}}>
+									حذف آگهی
+								</button>
 							</div>
 							<div className={styles.price}>
 								<p>
